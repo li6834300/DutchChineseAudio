71ec690142ca4a3fe65d042965f9dd4efc613146
mutingting520 <lizhien277@126.com>
2024-02-25 20:21:25 +0100

    prototype and demo

--- python/main.py
+++ python/main.py
@@ -6,39 +6,34 @@ from google.cloud import texttospeech_v1
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "static-groove-413415-af749aa41fa6.json"
 client = texttospeech_v1.TextToSpeechClient()
 
-UNIT = "3"
+UNIT = "9"
 
 text = '''<speak>
-Opstaan<break time="1000ms"/>Opstaan<break time="1000ms"/><voice name="cmn-CN-Wavenet-D">起床</voice><break time="1000ms"/>
-Slapen<break time="1000ms"/>Slapen<break time="1000ms"/><voice name="cmn-CN-Wavenet-D">睡觉</voice><break time="1000ms"/>
-Eten<break time="1000ms"/>Eten<break time="1000ms"/><voice name="cmn-CN-Wavenet-D">吃饭</voice><break time="1000ms"/>
-Lezen<break time="1000ms"/>Lezen<break time="1000ms"/><voice name="cmn-CN-Wavenet-D">读书</voice><break time="1000ms"/>
-Schrijven<break time="1000ms"/>Schrijven<break time="1000ms"/><voice name="cmn-CN-Wavenet-D">写字</voice><break time="1000ms"/>
-Tekenen<break time="1000ms"/>Tekenen<break time="1000ms"/><voice name="cmn-CN-Wavenet-D">画画</voice><break time="1000ms"/>
-Spelen<break time="1000ms"/>Spelen<break time="1000ms"/><voice name="cmn-CN-Wavenet-D">玩</voice><break time="1000ms"/>
-Werken<break time="1000ms"/>Werken<break time="1000ms"/><voice name="cmn-CN-Wavenet-D">工作</voice><break time="1000ms"/>
-Lachen<break time="1000ms"/>Lachen<break time="1000ms"/><voice name="cmn-CN-Wavenet-D">笑</voice><break time="1000ms"/>
-Huil<break time="1000ms"/>Huil<break time="1000ms"/><voice name="cmn-CN-Wavenet-D">哭</voice><break time="1000ms"/>
-Stoel<break time="1000ms"/>Stoel<break time="1000ms"/><voice name="cmn-CN-Wavenet-D">椅子</voice><break time="1000ms"/>
-Tafel<break time="1000ms"/>Tafel<break time="1000ms"/><voice name="cmn-CN-Wavenet-D">桌子</voice><break time="1000ms"/>
-Bed<break time="1000ms"/>Bed<break time="1000ms"/><voice name="cmn-CN-Wavenet-D">床</voice><break time="1000ms"/>
-Boek<break time="1000ms"/>Boek<break time="1000ms"/><voice name="cmn-CN-Wavenet-D">书</voice><break time="1000ms"/>
-Pen<break time="1000ms"/>Pen<break time="1000ms"/><voice name="cmn-CN-Wavenet-D">笔</voice><break time="1000ms"/>
-Papier<break time="1000ms"/>Papier<break time="1000ms"/><voice name="cmn-CN-Wavenet-D">纸</voice><break time="1000ms"/>
-Deur<break time="1000ms"/>Deur<break time="1000ms"/><voice name="cmn-CN-Wavenet-D">门</voice><break time="1000ms"/>
-Raam<break time="1000ms"/>Raam<break time="1000ms"/><voice name="cmn-CN-Wavenet-D">窗户</voice><break time="1000ms"/>
-Sleutel<break time="1000ms"/>Sleutel<break time="1000ms"/><voice name="cmn-CN-Wavenet-D">钥匙</voice><break time="1000ms"/>
-Tas<break time="1000ms"/>Tas<break time="1000ms"/><voice name="cmn-CN-Wavenet-D">包</voice><break time="1000ms"/>
-Ik ben blij<break time="1000ms"/>Ik ben blij<break time="1000ms"/><voice name="cmn-CN-Wavenet-D">我很高兴</voice><break time="1000ms"/>
-Ik ben verdrietig<break time="1000ms"/>Ik ben verdrietig<break time="1000ms"/><voice name="cmn-CN-Wavenet-D">我很伤心</voice><break time="1000ms"/>
-Ik ben moe<break time="1000ms"/>Ik ben moe<break time="1000ms"/><voice name="cmn-CN-Wavenet-D">我很累</voice><break time="1000ms"/>
-Ik heb honger<break time="1000ms"/>Ik heb honger<break time="1000ms"/><voice name="cmn-CN-Wavenet-D">我饿了</voice><break time="1000ms"/>
-Ik heb dorst<break time="1000ms"/>Ik heb dorst<break time="1000ms"/><voice name="cmn-CN-Wavenet-D">我渴了</voice><break time="1000ms"/>
-Ik ga slapen<break time="1000ms"/>Ik ga slapen<break time="1000ms"/><voice name="cmn-CN-Wavenet-D">我要睡觉了</voice><break time="1000ms"/>
-Tijd voor school<break time="1000ms"/>Tijd voor school<break time="1000ms"/><voice name="cmn-CN-Wavenet-D">上学时间</voice><break time="1000ms"/>
-Wat doen we vandaag?<break time="1000ms"/>Wat doen we vandaag?<break time="1000ms"/><voice name="cmn-CN-Wavenet-D">我们今天做什么？</voice><break time="1000ms"/>
-Kun je me helpen?<break time="1000ms"/>Kun je me helpen?<break time="1000ms"/><voice name="cmn-CN-Wavenet-D">你能帮我吗？</voice><break time="1000ms"/>
-Het is speeltijd<break time="1000ms"/>Het is speeltijd<break time="1000ms"/><voice name="cmn-CN-Wavenet-D">是玩耍时间</voice><break time="1000ms"/>
+Cultuur<break time="1000ms"/>Cultuur<break time="1000ms"/><voice name="cmn-CN-Wavenet-D">文化</voice><break time="1000ms"/>
+Feest<break time="1000ms"/>Feest<break time="1000ms"/><voice name="cmn-CN-Wavenet-D">节日</voice><break time="1000ms"/>
+Muziekfestival<break time="1000ms"/>Muziekfestival<break time="1000ms"/><voice name="cmn-CN-Wavenet-D">音乐节</voice><break time="1000ms"/>
+Kunst<break time="1000ms"/>Kunst<break time="1000ms"/><voice name="cmn-CN-Wavenet-D">艺术</voice><break time="1000ms"/>
+Tentoonstelling<break time="1000ms"/>Tentoonstelling<break time="1000ms"/><voice name="cmn-CN-Wavenet-D">展览</voice><break time="1000ms"/>
+Museum<break time="1000ms"/>Museum<break time="1000ms"/><voice name="cmn-CN-Wavenet-D">博物馆</voice><break time="1000ms"/>
+Theater<break time="1000ms"/>Theater<break time="1000ms"/><voice name="cmn-CN-Wavenet-D">剧院</voice><break time="1000ms"/>
+Film<break time="1000ms"/>Film<break time="1000ms"/><voice name="cmn-CN-Wavenet-D">电影</voice><break time="1000ms"/>
+Reizen<break time="1000ms"/>Reizen<break time="1000ms"/><voice name="cmn-CN-Wavenet-D">旅行</voice><break time="1000ms"/>
+Paspoort<break time="1000ms"/>Paspoort<break time="1000ms"/><voice name="cmn-CN-Wavenet-D">护照</voice><break time="1000ms"/>
+Visum<break time="1000ms"/>Visum<break time="1000ms"/><voice name="cmn-CN-Wavenet-D">签证</voice><break time="1000ms"/>
+Vliegticket<break time="1000ms"/>Vliegticket<break time="1000ms"/><voice name="cmn-CN-Wavenet-D">飞机票</voice><break time="1000ms"/>
+Toerist<break time="1000ms"/>Toerist<break time="1000ms"/><voice name="cmn-CN-Wavenet-D">游客</voice><break time="1000ms"/>
+Gids<break time="1000ms"/>Gids<break time="1000ms"/><voice name="cmn-CN-Wavenet-D">导游</voice><break time="1000ms"/>
+Vakantie<break time="1000ms"/>Vakantie<break time="1000ms"/><voice name="cmn-CN-Wavenet-D">假期</voice><break time="1000ms"/>
+Taal<break time="1000ms"/>Taal<break time="1000ms"/><voice name="cmn-CN-Wavenet-D">语言</voice><break time="1000ms"/>
+Woordenboek<break time="1000ms"/>Woordenboek<break time="1000ms"/><voice name="cmn-CN-Wavenet-D">词典</voice><break time="1000ms"/>
+Vertaler<break time="1000ms"/>Vertaler<break time="1000ms"/><voice name="cmn-CN-Wavenet-D">翻译</voice><break time="1000ms"/>
+Cursus<break time="1000ms"/>Cursus<break time="1000ms"/><voice name="cmn-CN-Wavenet-D">课程</voice><break time="1000ms"/>
+Leren<break time="1000ms"/>Leren<break time="1000ms"/><voice name="cmn-CN-Wavenet-D">学习</voice><break time="1000ms"/>
+Wil je een museum bezoeken?<break time="1000ms"/>Wil je een museum bezoeken?<break time="1000ms"/><voice name="cmn-CN-Wavenet-D">你想去参观博物馆吗？</voice><break time="1000ms"/>
+Laten we naar een filmfestival gaan<break time="1000ms"/>Laten we naar een filmfestival gaan<break time="1000ms"/><voice name="cmn-CN-Wavenet-D">让我们去参加电影节</voice><break time="1000ms"/>
+Ik wil mijn taalvaardigheid verbeteren<break time="1000ms"/>Ik wil mijn taalvaardigheid verbeteren<break time="1000ms"/><voice name="cmn-CN-Wavenet-D">我想提高我的语言能力</voice><break time="1000ms"/>
+Heb je een aanbeveling voor een goed boek?<break time="1000ms"/>Heb je een aanbeveling voor een goed boek?<break time="1000ms"/><voice name="cmn-CN-Wavenet-D">你有好书推荐吗？</voice><break time="1000ms"/>
+Ik plan een reis naar het buitenland<break time="1000ms"/>Ik plan een reis naar het buitenland<break time="1000ms"/><voice name="cmn-CN-Wavenet-D">我计划去国外旅行</voice><break time="1000ms"/>
 </speak>'''
 
 synthsis_input = texttospeech_v1.SynthesisInput(ssml=text)
